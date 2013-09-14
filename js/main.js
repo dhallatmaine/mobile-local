ca4d8d4c8e191add8437cffbad8935d3f3b3e5c9
Derrick Hall <dhall@bluetarp.com>
2013-09-14 19:59:43 -0400

    Move weather scripts to seperate js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+var zipcode = '04106';
+$(document).ready(function(){
+  $.ajax({
+    url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20location%3D' + zipcode + '&format=json&diagnostics=true&callback=',
+    dataType: 'json',
+    success: function(data){
+      setWeather(data);
+    }
+  });
+})
 
+function setWeather(data) {
+  var item = data['query']['results']['channel']['item'];
+  var condition = item['condition'];
+  var text = '<p>' + item['title'] + '<br />';
+  text += condition['date'] + '<br />';
+  text += condition['text'];
+  text += '<img src="' + item['description'].substring(11,48) + '" /> ';
+  text += condition['temp'] + ' F&deg;</p>';
+  $('#weather').html(text);
+}
